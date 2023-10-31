4b6c487f71c8a5790027a3ba569b6c2f5595395e
Aniket2503200 <93081630+Aniket2503200@users.noreply.github.com>
2023-10-31 15:04:06 +0530

    Add files via upload

---
+++ 13_Assignment/Question5.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Display(int Arr[],int iLength){
+    int iCnt=0;
+
+        printf("Elements which are multiple of 11 : \n");
+
+    for(iCnt=0;iCnt<iLength;iCnt++){
+        if((Arr[iCnt]%11)==0){
+            printf("%d\t",Arr[iCnt]);
+        }
+    }
+
+}
+
+
+int main(){
+    int iSize=0;
+    int *p=NULL;
+    int iCnt=0;
+
+    printf("Enter the elements that you want to entered :\n");
+    scanf("%d",&iSize);
+
+    p=(int*)malloc(iSize*sizeof(int));
+    if(p==NULL){
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Dynamic memory is allocated......\n");
+    printf("Enter the %d elements :\n",iSize);
+
+    for(iCnt=0;iCnt<iSize;iCnt++){
+        printf("Enter the element %d\n",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    Display(p,iSize);
+
+    free(p);
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
